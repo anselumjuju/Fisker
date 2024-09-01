d624ae6c41537778a87e3ee4927ee25cc3d7aaaf
juju-anselum <anselumjuju@gmail.com>
2024-09-01 16:27:20 +0530

    Mission section responsiveness

--- src/containers/Mission.jsx
+++ src/containers/Mission.jsx
@@ -27,15 +27,15 @@ const Mission = () => {
 	}, []);
 
 	return (
-		<div className="w-[90%] h-[80vh] lg:h-[100vh] mx-auto relative rounded-lg overflow-clip" ref={containerRef}>
+		<div className="w-[90%] h-[40vh] lg:h-[100vh] mx-auto relative rounded-lg overflow-clip" ref={containerRef}>
 
 			<div className="w-full px-[10%] mx-auto absolute top-10 flex items-stretch justify-between">
 				<div>
 					<p className="font-medium text-lg">Our Mission</p>
 					<h1 className="mt-1 lg:mt-2 font-extrabold text-2xl lg:text-6xl font-orbitron">A Clean Future For All</h1>
 				</div>
-				<div className="hidden lg:block p-8 bg-white rounded-full transition-all duration-500 ease-in-out hover:scale-110">
-					<img src={NavigateSvg} alt="" className="w-8" />
+				<div className="hidden lg:block p-8 bg-white rounded-full transition-all duration-500 ease-in-out hover:scale-125 group">
+					<img src={NavigateSvg} alt="" className="w-8 transition-all duration-200 ease-in-out group-hover:rotate-45" />
 				</div>
 			</div>
 
